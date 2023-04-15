5aad8d9d8c47a1e3b65fc38b07f0434423fda012
Slava Volkov <sv99@inbox.ru>
2023-04-15 14:30:39 +0300

    mispelling

--- Formula/fb2converter.rb
+++ Formula/fb2converter.rb
@@ -2,7 +2,7 @@ class Apg < Formula
   desc "Unified converter of FB2 files into epub2, kepub, mobi and azw3 formats"
   homepage "https://github.com/rupor-github/fb2converter"
   url "https://github.com/rupor-github/fb2converter/archive/refs/tags/v1.70.0.tar.gz"
-  licence "GNU General Public License v3.0"
+  license "GNU General Public License v3.0"
   sha256 "3b0ece67d504bdf73736dea430223ce82aac813293cc91f905843b750b8a0fdd"
   head "https://github.com/rupor-github/fb2converter.git", branch: "master"
 

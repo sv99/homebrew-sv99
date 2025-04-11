a3acc65a95bd9b8d9b08304d725872c841f08a8a
sv99 <sv99@inbox.ru>
2025-04-11 10:53:47 +0300

    Update fb2converter.rb to v1.77.2

--- Formula/fb2converter.rb
+++ Formula/fb2converter.rb
@@ -3,7 +3,7 @@ class Fb2converter < Formula
   # for build from source need additional steps
   desc "Unified converter of FB2 files into epub2, kepub, mobi and azw3 formats"
   homepage "https://github.com/rupor-github/fb2converter"
-  version "1.75.1"
+  version "1.77.2"
   url "https://github.com/rupor-github/fb2converter/releases/download/v#{version}/fb2c-darwin-amd64.zip"
   license "GNU General Public License v3.0"
   sha256 "f27f957f52bb6e9453ee4f3c5ef0052e1cb7b9c8f12ca0723f9df90cafce49fe"
